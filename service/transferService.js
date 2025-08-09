0688f414d0579a74d50bd537a8919bceffee6f7c
Julio de Lima <juliodelimas@Julios-MBP-2.lan>
2025-08-09 15:55:58 -0400

    Corrigindo o erro que eu mesmo criei

--- service/transferService.js
+++ service/transferService.js
@@ -4,7 +4,7 @@ const { transfers } = require('../model/transferModel');
 function transfer({ from, to, value }) {
   const sender = users.find(u => u.username === from);
   const recipient = users.find(u => u.username === to);
-  if (!sender || !recipient) throw new Error('Usuário remetente, destinatário não encontrado');
+  if (!sender || !recipient) throw new Error('Usuário remetente ou destinatário não encontrado');
   if (sender.saldo < value) throw new Error('Saldo insuficiente');
   const isFavorecido = sender.favorecidos && sender.favorecidos.includes(to);
   if (!isFavorecido && value >= 5000) throw new Error('Transferência acima de R$ 5.000,00 só para favorecidos');
