976ca05b0b442d1511e918cab22317e3a4219218
Julio de Lima <juliodelimas@Mac.lan>
2025-08-23 13:17:50 -0400

    Adicionando autenticação nos testes de Controller e voltando a roda-los na pipeline

--- test/controller/transferController.test.js
+++ test/controller/transferController.test.js
@@ -12,9 +12,22 @@ const transferService = require('../../service/transferService');
 // Testes
 describe('Transfer Controller', () => {
     describe('POST /transfers', () => {
+
+        beforeEach(async () => {
+            const respostaLogin = await request(app)
+                .post('/users/login')
+                .send({
+                    username: 'julio',
+                    password: '123456'
+                });
+
+            token = respostaLogin.body.token;
+        })
+
         it('Quando informo remetente e destinatario inexistentes recebo 400', async () => {
             const resposta = await request(app)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     from: "julio",
                     to: "priscila",
@@ -32,6 +45,7 @@ describe('Transfer Controller', () => {
 
             const resposta = await request(app)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     from: "julio",
                     to: "priscila",
@@ -39,10 +53,7 @@ describe('Transfer Controller', () => {
                 });
             
             expect(resposta.status).to.equal(400);
-            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado')
-
-            // Reseto o Mock
-            sinon.restore();
+            expect(resposta.body).to.have.property('error', 'Usuário remetente ou destinatário não encontrado');
         });
 
         it('Usando Mocks: Quando informo valores válidos eu tenho sucesso com 201 CREATED', async () => {
@@ -57,6 +68,7 @@ describe('Transfer Controller', () => {
 
             const resposta = await request(app)
                 .post('/transfers')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     from: "julio",
                     to: "priscilaaaaaaaaaaa",
@@ -75,10 +87,12 @@ describe('Transfer Controller', () => {
             // expect(resposta.body).to.have.property('from', 'julio');
             // expect(resposta.body).to.have.property('to', 'priscila');
             // expect(resposta.body).to.have.property('value', 100);
+        });
 
+        afterEach(() => {
             // Reseto o Mock
             sinon.restore();
-        });
+        })
     });
 
     describe('GET /transfers', () => {
